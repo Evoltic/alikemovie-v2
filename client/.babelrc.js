79c7b72a08170b2e880e2daf4dd639426b7b46d6
Rinat R <evolticdev@gmail.com>
2021-09-05 10:13:50 +0500

    Change babel presets: make them (preset-react and preset-env) the same for any environment

--- client/.babelrc.js
+++ client/.babelrc.js
@@ -1,8 +1,4 @@
 module.exports = {
-  presets: [
-    process.env.NODE_ENV === 'test'
-      ? '@babel/preset-env'
-      : '@babel/preset-react'
-  ],
+  presets: ['@babel/preset-env','@babel/preset-react'],
   plugins: ['@babel/plugin-transform-runtime']
 }
