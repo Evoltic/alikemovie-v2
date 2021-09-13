d7e34b3f81efbe5df1fd356adb6f8edc21249cd7
Rinat R <evolticdev@gmail.com>
2021-09-13 13:29:02 +0500

    Add an option to run the project in a development mode

---
+++ client/devServer.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const pages = require('./src/pages')
+
+let webpackConfigs = require('./webpackConfig')
+
+webpackConfigs = webpackConfigs.map((config) => {
+  return {
+    ...config,
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+    optimization: {},
+  }
+})
+
+function start() {
+  const app = express()
+  const compiler = webpack(webpackConfigs)
+
+  app.use(
+    webpackDevMiddleware(compiler, {
+      serverSideRender: true,
+    })
+  )
+
+  app.use((req, res) => {
+    const outputFileSystem = res.locals.webpack.devMiddleware.outputFileSystem
+
+    const page = pages.find((page) => page.route.test(req.path))
+    if (!page) return
+
+    res.send(
+      outputFileSystem.readFileSync(__dirname + `/dist/${page.source}.html`, {
+        encoding: 'utf8',
+      })
+    )
+  })
+
+  const port = 8000
+  app.listen(port, () => console.log(`\nthe server is listening on ${port} \n`))
+}
+start()
