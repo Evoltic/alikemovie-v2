f99fad20d97af8f80fe1bf1c95f180effcb014ee
Rinat R <evolticdev@gmail.com>
2021-09-16 16:10:06 +0500

    Style MovieCard component and add markup

--- client/src/components/movieCard/markup.js
+++ client/src/components/movieCard/markup.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import './index.scss'
+import placeholderImage from '/assets/images/placeholder_264x330.jpg'
 
 export const Markup = (props) => {
-  const { title, startyear } = props
+  const { title, startyear, endyear, genres } = props
 
   return (
     <div className="movie-card">
-      <p className="movie-card__title">{title}</p>
-      <p className="movie-card__year">{startyear}</p>
+      <div className="movie-card__aside">
+        <img className="movie-card__cover" src={placeholderImage} />
+      </div>
+      <div className="movie-card__main">
+        <h2 className="movie-card__title">
+          <span className="movie-card__title-name">{title}</span>
+          <span className="movie-card__title-year">
+            {(endyear ? `${endyear} - ` : '') + startyear}
+          </span>
+        </h2>
+        <div className="movie-card__chips">
+          {genres &&
+            genres.map((genre) => (
+              <p className="movie-card__chip" key={genre}>
+                {genre}
+              </p>
+            ))}
+        </div>
+      </div>
     </div>
   )
 }
