b1ced6787d2b7ec6fcf73ef8b6d9d2e628b49621
Rinat R <evolticdev@gmail.com>
2021-09-16 16:11:37 +0500

    Add basic styles for SimilarMovies component and resolve the issue with key attr

--- client/src/components/similarMovies/markup.js
+++ client/src/components/similarMovies/markup.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { MovieCard } from '/components/movieCard'
+import './index.scss'
 
 export const Markup = (props) => {
   const { items = [], isPending, isNotFound, isUnknownError } = props
@@ -12,8 +13,8 @@ export const Markup = (props) => {
         <p className="similar-movies__not-found">unknown error</p>
       )}
       {items.map((item) => (
-        <div className="similar-movies__item">
-          <MovieCard {...item} key={item.id} />
+        <div className="similar-movies__item" key={item.id}>
+          <MovieCard {...item} />
         </div>
       ))}
     </div>
