290611cc60c12ac7da30e024fb1ee03193b4dd8f
Rinat R <evolticdev@gmail.com>
2021-09-19 19:14:33 +0500

    Improve similar movies search: distinct actors and authors by weight, take into account genres, filter by a type

--- server/src/services/similarMovies/instance.js
+++ server/src/services/similarMovies/instance.js
@@ -1,41 +1,67 @@
 const { postgreSql } = require('../postgreSql/instance')
 
 // todo:
-//  distinct actors and authors by weight;
 //  limit the result;
 //  remove the first row from the result;
 //  cache the result;
+//  improve query speed;
 
-const similarMovies = new class {
-  findById(id) {
+const similarMovies = new (class {
+  findById(id, types = ['movie', 'tvSeries']) {
     return postgreSql
       .query(
-        `   
+        `      
         WITH movieCastAndCrew AS (
           SELECT * FROM moviescastandcrew
           WHERE moviescastandcrew.movieid = $1
-        ), sameCastAndCrewMovies AS (
+        ), sameCastAndCrew AS (
           SELECT
-            moviesCastAndCrew.movieId,
-            1 / (ABS(movieCastAndCrew.order - moviesCastAndCrew.order) + movieCastAndCrew.order + moviesCastAndCrew.order)^2
-                as score
+            moviesCastAndCrew.movieId,      
+            (CASE 
+              WHEN movieCastAndCrew.category IN ('actor', 'actress') 
+                THEN (ABS(movieCastAndCrew.order - moviesCastAndCrew.order) + movieCastAndCrew.order + moviesCastAndCrew.order)^(-1)
+              WHEN movieCastAndCrew.category IN ('director', 'writer') 
+                THEN (2 + ABS(movieCastAndCrew.order - moviesCastAndCrew.order))^(-1)
+              ELSE 
+                0
+            END) as scoreForCrew
           FROM movieCastAndCrew
           LEFT JOIN moviesCastAndCrew
             ON movieCastAndCrew.castandcrewid = moviesCastAndCrew.castandcrewid
             AND movieCastAndCrew.category = moviesCastAndCrew.category
-        ), sameCastAndCrewMoviesUnique AS (
-            SELECT movieId, SUM (score) score FROM sameCastAndCrewMovies
+        ), sameByCastAndCrewMoviesBrief AS (
+            SELECT movieId, SUM (scoreForCrew) as scoreForCrew 
+            FROM sameCastAndCrew
             GROUP BY movieid
-            ORDER BY score DESC
+        ), sameByCastAndCrewMoviesExtended AS (
+          SELECT * FROM sameByCastAndCrewMoviesBrief
+          LEFT JOIN movies ON movies.id = sameByCastAndCrewMoviesBrief.movieid
+        ), sameByCastAndCrewMovies AS (
+          SELECT
+            *,
+            (
+              SELECT COUNT(*)
+              FROM 
+                unnest(
+                    (SELECT genres FROM movies WHERE movies.id = $1),
+                    sameByCastAndCrewMoviesExtended.genres
+                ) AS t(a, b)
+              WHERE a=b
+            )*0.5 AS scoreForGenres
+          FROM sameByCastAndCrewMoviesExtended
+          WHERE type = ANY ($2)
         )
-        
-        SELECT * FROM sameCastAndCrewMoviesUnique
-        LEFT JOIN movies ON movies.id = sameCastAndCrewMoviesUnique.movieid;
+
+        SELECT 
+          *, 
+          (sameByCastAndCrewMovies.scoreForCrew + scoreForGenres) AS score
+        FROM sameByCastAndCrewMovies
+        ORDER BY score DESC;
     `,
-        [id]
+        [id, types]
       )
-      .then(result => result.rows)
+      .then((result) => result.rows)
   }
-}
+})()
 
 module.exports = { similarMovies }
