8d1d82cc6b56f1da7882726d9b52554d71ea86e4
Rinat R <evolticdev@gmail.com>
2021-10-01 17:39:30 +0500

    Create assetsManager service

---
+++ server/src/services/assetsManager/index.js
@@ -0,0 +1,84 @@
+class AssetsManager {
+  constructor(optional = {}) {
+    const {
+      groupName = 'common',
+      fs = require('fs'),
+      availableBytes = Infinity,
+      handleLimitError,
+    } = optional
+
+    this.fs = fs
+    this.basePath = __dirname + `/assets/${groupName}-`
+
+    this.availableBytes = availableBytes
+    this.handleLimitError = handleLimitError
+  }
+
+  async saveAsset(fileName, data) {
+    const path = this.basePath + `${fileName}`
+    const dataSize = data.length
+
+    if (this.availableBytes - dataSize < 0) {
+      const e = new Error(
+        `can't save the asset: exceed the available bytes limit`
+      )
+
+      if (!this.handleLimitError) throw e
+
+      const shouldAssetBeSaved = this.handleLimitError(e)
+      if (!shouldAssetBeSaved) return
+    }
+
+    this.availableBytes -= dataSize
+    return new Promise((resolve, reject) => {
+      this.fs.writeFile(path, data, (err) => {
+        err ? reject(err) : resolve()
+      })
+    })
+  }
+
+  async findAsset(fileName, cacheTimeMs) {
+    const path = this.basePath + `${fileName}`
+
+    const stats = await new Promise((resolve) => {
+      this.fs.stat(path, (err, stats) => {
+        err ? resolve(null) : resolve(stats)
+      })
+    })
+    if (!stats) return null
+
+    if (Date.now() - new Date(stats.mtime).getTime() >= cacheTimeMs) {
+      return null
+    }
+
+    return new Promise((resolve, reject) => {
+      this.fs.readFile(path, (err, data) => {
+        err ? reject(err) : resolve(data)
+      })
+    })
+  }
+
+  async callWithCacheLookup(
+    resolveData,
+    fileName,
+    cacheTimeMs = Infinity,
+    modifiers = {}
+  ) {
+    const {
+      encode = async (data) => data,
+      decode = async (buffer) => buffer.toString(),
+    } = modifiers
+
+    let result
+
+    result = await this.findAsset(fileName, cacheTimeMs)
+    if (result) return decode(result)
+
+    result = await resolveData()
+    await this.saveAsset(fileName, await encode(result))
+
+    return result
+  }
+}
+
+module.exports = { AssetsManager }
